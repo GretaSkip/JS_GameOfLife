f799104995bda5dad0b39e51e24b4e10836af8f8
Greta Skipare <gretchfurlined@gmail.com>
2021-06-07 18:33:13 +0300

    Initial commit

---
+++ gameoflife.js
@@ -0,0 +1,13 @@
+console.log("hello");
+
+let area = [];
+
+for (let i = 0; i < 5; i++) {
+    area.push([]);
+    for (let a = 0; a < 5; a++) {
+        area[i].push(a);
+        
+    }
+    
+}
+console.log(area);
